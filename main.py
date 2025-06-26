4594031bfbcedbad960336c4c84ce8ad2fcd1249
AndresGovea <goveaandres11@gmail.com>
2025-06-25 20:12:50 -0600

    Trabajando con caracteres

---
+++ main.py
@@ -0,0 +1,21 @@
+
+# Pintamos en terminal
+print('Hello world!')
+
+# Guardamos contenido en un variable
+hace = 'Hola Que Hace'
+
+# Volvemos el contenido a mayúsculas
+print(hace.upper())
+
+# Volvemos el contenido a minúsculas
+print(hace.lower())
+
+# La función index nos devuelve el primer carácter
+print(hace.index('Q'))
+
+# Agarramos el quinto carácter de nuestra variable
+print(hace[5])
+
+#La función len() nos devuelve la longitud en caracteres de la variable
+print(len(hace))
